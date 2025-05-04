eac412d878df6ec6bcd868434523b740bef8d9ca
LIU-Xnd <lqxnds@foxmail.com>
2025-05-04 16:15:30 +0800

    FIX bug: path with whitespaces.

--- src/lazyls.sh
+++ src/lazyls.sh
@@ -2,9 +2,9 @@
 
 $lz_path_/.venv/bin/python $lz_path_/src/lazyls_print.py
 
-target_path=$($lz_path_/.venv/bin/python $lz_path_/src/lazyls_parse.py | grep '::LZOUT::' | cut -f 2)
+target_path="$($lz_path_/.venv/bin/python $lz_path_/src/lazyls_parse.py | grep '::LZOUT::' | cut -f 2)"
 
-if [ -d $target_path ]; then
+if [ -d "$target_path" ]; then
   cd "$target_path"
 else
   echo "$target_path"
