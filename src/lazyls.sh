88241be77295b0c892369a094ded04a0d6b1a196
X. Liu <87137538+LIU-Xnd@users.noreply.github.com>
2025-05-04 15:57:31 +0800

    FIX bug: handle path with spaces

--- src/lazyls.sh
+++ src/lazyls.sh
@@ -5,7 +5,7 @@ $lz_path_/.venv/bin/python $lz_path_/src/lazyls_print.py
 target_path=$($lz_path_/.venv/bin/python $lz_path_/src/lazyls_parse.py | grep '::LZOUT::' | cut -f 2)
 
 if [ -d $target_path ]; then
-  cd $target_path
+  cd "$target_path"
 else
-  echo $target_path
+  echo "$target_path"
 fi
